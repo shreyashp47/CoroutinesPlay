e74b75b2a9a1338a74ccb23726b65f848fbbd7c2
Shreyash <shreyashp47@gmail.com>
2024-01-17 08:07:57 +0530

    display result after network call on main thread

--- app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -3,30 +3,40 @@ package com.example.coroutinesdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    private lateinit var  dummy : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch{
+        dummy = findViewById(R.id.dummy)
+
+        GlobalScope.launch(Dispatchers.IO){
            val result = doNetworkCall()
+
+            Log.d(TAG, "GlobalScope: ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                Log.d(TAG, "withContext: ${Thread.currentThread().name}")
+                dummy.text = result
+            }
             Log.d(TAG, "onCreate: $result")
 
         }
-        Log.d(TAG, "onCreate: ${Thread.currentThread().name}")
     }
 
 
     private suspend fun doNetworkCall(): String{
-        delay(300L)
-        Log.d(TAG, "doNetworkCall: ${Thread.currentThread().name}")
+        delay(5000L)
         return "Hello Network Call!"
     }
 }
